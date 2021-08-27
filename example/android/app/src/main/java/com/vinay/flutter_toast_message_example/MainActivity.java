9e9800e5c16950cd0936bcf1289c8262e54b5839
Vinay <vinay.m@karza.in>
2021-08-27 21:00:38 +0530

    aadhaar sdk impl done

--- example/android/app/src/main/java/com/vinay/flutter_toast_message_example/MainActivity.java
+++ example/android/app/src/main/java/com/vinay/flutter_toast_message_example/MainActivity.java
@@ -1,6 +1,21 @@
 package com.vinay.flutter_toast_message_example;
 
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+//        super.configureFlutterEngine(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine) ;// add this line
+
+    }
+
 }
